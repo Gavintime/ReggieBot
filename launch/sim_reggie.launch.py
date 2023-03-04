0545677703cc55ee1065f61354e843d17f8c5204
Gavintime <16593223+Gavintime@users.noreply.github.com>
2023-03-03 20:56:23 -0800

    Increase gazebo clock to 120 also delay controllers starting up until after the spawner finishes

--- launch/sim_reggie.launch.py
+++ launch/sim_reggie.launch.py
@@ -3,11 +3,15 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessExit
 from launch_ros.actions import Node
+
 
 def generate_launch_description():
 
     pkg_path = os.path.join(get_package_share_directory('reggie_bot'))
+    gazebo_params_file = os.path.join(pkg_path, 'config', 'gazebo_params.yaml')
 
     # reggie's rsp launch file
     rsp_reggie_launch = IncludeLaunchDescription(
@@ -19,7 +23,11 @@ def generate_launch_description():
     # launch gazebo sim
     gazebo_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
+            get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+        launch_arguments={
+            # 'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file
+            'params_file': gazebo_params_file
+        }.items()
     )
 
     # spawn reggie using it's desc from the rsp
@@ -34,12 +42,23 @@ def generate_launch_description():
                               arguments=['diff_cont'])
 
     joint_broad_spawner = Node(package='controller_manager',
-                              executable='spawner',
-                              arguments=['joint_broad'])
+                               executable='spawner',
+                               arguments=['joint_broad'])
+
+    # delay starting controllers until the robot has been spawned in gazebo
+    delayed_diff_drive_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(target_action=spawn_entity,
+                                    on_exit=[diff_drive_spawner]))
+
+    delayed_joint_broad_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(target_action=spawn_entity,
+                                    on_exit=[joint_broad_spawner]))
+
+    # Replace the diff_drive_spawner in the final return with delayed_diff_drive_spawner
 
     # launch
     return LaunchDescription([rsp_reggie_launch,
                               gazebo_launch,
                               spawn_entity,
-                              diff_drive_spawner,
-                              joint_broad_spawner])
+                              delayed_diff_drive_spawner,
+                              delayed_joint_broad_spawner])
