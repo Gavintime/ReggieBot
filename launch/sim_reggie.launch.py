b2913984fd06279c8e28dfee0936610fa501fd09
Gavintime <16593223+Gavintime@users.noreply.github.com>
2023-03-04 14:42:44 -0800

    integrate slam_toolkit

--- launch/sim_reggie.launch.py
+++ launch/sim_reggie.launch.py
@@ -12,11 +12,14 @@ def generate_launch_description():
 
     pkg_path = os.path.join(get_package_share_directory('reggie_bot'))
     gazebo_params_file = os.path.join(pkg_path, 'config', 'gazebo_params.yaml')
+    slam_params_file = os.path.join(pkg_path, 'config',
+                                    'mapper_params_online_async.yaml')
+    rviz_config_file = os.path.join(pkg_path, 'config', 'reggiebot.rviz')
 
     # reggie's rsp launch file
     rsp_reggie_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
-            pkg_path, 'launch'), '/rsp_reggie.launch.py']),
+            pkg_path, 'launch', 'rsp_reggie.launch.py')]),
         launch_arguments={'use_sim_time': 'true'}.items(),
     )
 
@@ -24,10 +27,7 @@ def generate_launch_description():
     gazebo_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-        launch_arguments={
-            # 'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file
-            'params_file': gazebo_params_file
-        }.items()
+        launch_arguments={'params_file': gazebo_params_file}.items()
     )
 
     # spawn reggie using it's desc from the rsp
@@ -36,29 +36,37 @@ def generate_launch_description():
                                    '-entity', 'my_reggiebot'],
                         output='screen')
 
-    # TODO: wait for spawn entity to finish
+    # delay starting controllers until the robot has been spawned in gazebo
     diff_drive_spawner = Node(package='controller_manager',
                               executable='spawner',
                               arguments=['diff_cont'])
-
-    joint_broad_spawner = Node(package='controller_manager',
-                               executable='spawner',
-                               arguments=['joint_broad'])
-
-    # delay starting controllers until the robot has been spawned in gazebo
     delayed_diff_drive_spawner = RegisterEventHandler(
         event_handler=OnProcessExit(target_action=spawn_entity,
                                     on_exit=[diff_drive_spawner]))
 
+    joint_broad_spawner = Node(package='controller_manager',
+                               executable='spawner',
+                               arguments=['joint_broad'])
     delayed_joint_broad_spawner = RegisterEventHandler(
         event_handler=OnProcessExit(target_action=spawn_entity,
                                     on_exit=[joint_broad_spawner]))
 
-    # Replace the diff_drive_spawner in the final return with delayed_diff_drive_spawner
+    # slam toolbox online async launch file
+    slam_toolbox_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(get_package_share_directory(
+            'slam_toolbox'), 'launch', 'online_async_launch.py')),
+        launch_arguments={'use_sim_time': 'true',
+                          'slam_params_file': slam_params_file}.items()
+    )
+
+    rviz2_node = Node(package='rviz2', executable='rviz2',
+                      name='rviz2', arguments={'-d': rviz_config_file}.items())
 
     # launch
     return LaunchDescription([rsp_reggie_launch,
                               gazebo_launch,
                               spawn_entity,
                               delayed_diff_drive_spawner,
-                              delayed_joint_broad_spawner])
+                              delayed_joint_broad_spawner,
+                              slam_toolbox_launch,
+                              rviz2_node])
